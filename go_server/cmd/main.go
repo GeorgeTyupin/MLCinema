b5f642746bb189804adef94ad13f67424d3b7a8f
GeorgeTyupin <GeorgyTyupin@ya.ru>
2025-06-30 23:56:44 +0300

    feat: Подключил бд PostgreSQL. Создал .env и .gitignore

--- go_server/cmd/main.go
+++ go_server/cmd/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"io"
+	"log"
+	"os"
 
 	"github.com/GeorgeTyupin/MLCinema/go_server/internal/routers"
 	"github.com/labstack/echo/v4"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type TemplateRenderer struct {
 	templates *template.Template
 }
+
+var DB *gorm.DB
 
 func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	if viewContext, isMap := data.(map[string]interface{}); isMap {
@@ -21,6 +30,26 @@ func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c 
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Ошибка подключения к базе данных: %v", err)
+	}
+	DB = db
+
 	e := echo.New()
 	renderer := &TemplateRenderer{
 		templates: template.Must(template.ParseGlob("go_server/templates/*.html")),
