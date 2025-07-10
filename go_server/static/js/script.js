e0a1d1022fab4a5eb561edc61015a3094aa7b4a6
GeorgeTyupin <GeorgyTyupin@ya.ru>
2025-07-11 01:16:58 +0300

    feat: Добавил фильтрацию по категориям

--- go_server/static/js/script.js
+++ go_server/static/js/script.js
@@ -8,25 +8,73 @@ function customHeightTextarea() {
     });
 }
 
+function activeCategory() {
+    let width = $(this).width();
+    let height = $(this).height();
+
+    if ($(this).hasClass('active-category')) {
+        $(this).removeClass('active-category');
+        $(this).width(width - 50);
+        $(this).height(height - 10);
+        $(".film-row").eq(0).children().remove();
+        $(".section-title").eq(1).removeClass('hide');
+        $(".film-row").eq(1).removeClass('hide');
+        $(".film-row").eq(0).append(`                    
+            <div class="film-card skeleton" id=""><img src="" alt=""></div>
+            <div class="film-card skeleton" id=""><img src="" alt=""></div>
+            <div class="film-card skeleton" id=""><img src="" alt=""></div>
+            <div class="film-card skeleton" id=""><img src="" alt=""></div>
+            <div class="film-card skeleton" id=""><img src="" alt=""></div>
+        `);
+        renderFilmPosters();
+    } else {
+        $('.menu-item').each(function () {
+            if ($(this).hasClass('active-category')) {
+                console.log(this)
+                $(this).removeClass('active-category');
+                $(this).width(width);
+                $(this).height(height);
+            }
+        });
+        $(this).addClass('active-category');
+        $(this).width(width + 50);
+        $(this).height(height + 10);
+        renderFilmByCategory($(this).children().eq(0).text(), $(this).attr("id").slice(7));
+    }
+}
+
+function renderFilmByCategory(cat_text, cat_id) {
+    $(".section-title").eq(1).addClass('hide');
+    $(".film-row").eq(1).addClass('hide');
+    $(".section-title").eq(0).text(cat_text)
+    $(".film-row").eq(0).children().remove();
+    films.forEach( (film) => {
+        film.categories.forEach((cat) => {
+            if (cat.id == cat_id) {
+                $(".film-row").eq(0).append(`<div class="film-card" id="film_id-${film.id}"><img src="${film.imagePath}" alt=""></div>`);
+            }
+        });
+    });
+}
+
 function renderFilmPosters() {
     if (!films.length) {
         console.warn("Нет фильмов для отображения");
-        return;
     }
 
     $(".film-card").each(function(index, row) {
         if (films[index]) {
             $(row).removeClass("skeleton");
             let cardImg = $(row).children()[0];
-            cardImg.src = `..${films[index].imagePath}`;
-            $(row).attr("id", films[index].id);
+            cardImg.src = `${films[index].imagePath}`;
+            $(row).attr("id", `film_id-${films[index].id}`);
         }
     });
 }
 
 function renderCategories(categories) {
     categories.forEach(category => {
-        $(".menu").append(`<li class="menu-item" id="${category.id}"><span>${category.name}</span></li>`);
+        $(".menu").append(`<li class="menu-item" id="cat_id-${category.id}"><span>${category.name}</span></li>`);
     });
 
     let maxWidth = 0;
@@ -37,8 +85,6 @@ function renderCategories(categories) {
             maxWidth = width;
         }
     });
-    
-    console.log(maxWidth)
 
     $('.menu-item').each(function () {
         $(this).width(maxWidth);
@@ -46,12 +92,13 @@ function renderCategories(categories) {
 }
 
 function redirectToFilm() {
-    window.location.href = `/film?film_id=${this.id}`;
+    window.location.href = `/film?film_id=${this.id.slice(8)}`;
 }
 
 function getFilms() {
     $.post("/api/get-films", function(response) {
-        films = response;       
+        films = response;   
+        console.log(films)    
         renderFilmPosters();       
     });
 }
@@ -67,6 +114,7 @@ function main() {
     getCategories();
     customHeightTextarea();
     $(".film-card").click(redirectToFilm);
+    $(".menu").on("click", ".menu-item", activeCategory);
 }
 
 main();
