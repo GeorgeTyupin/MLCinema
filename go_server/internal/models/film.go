cb5d419813cbc3ac1595f7b5886c457bed0ccdb6
GeorgeTyupin <GeorgyTyupin@ya.ru>
2025-07-11 15:38:32 +0300

    refactor: Удалил ненужный атрибут - жанр у фильмов

--- go_server/internal/models/film.go
+++ go_server/internal/models/film.go
@@ -5,7 +5,6 @@ type Film struct {
 	Title       string      `json:"title" gorm:"not null"`
 	Year        uint        `json:"year"`
 	Country     string      `json:"country"`
-	Genre       string      `json:"genre"`
 	ImagePath   string      `json:"imagePath"`
 	Actors      []*Actor    `json:"actors" gorm:"many2many:film_actors"`
 	Categories  []*Category `json:"categories" gorm:"many2many:film_categories"`
